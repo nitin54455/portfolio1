e8dc11317dc25f02672dad2508db4d706e39fe49
nitin singh <119619544+nitin54455@users.noreply.github.com>
2025-04-23 01:58:35 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true, // disables Next.js image optimization
+  },
 }
 
-module.exports = nextConfig 
+module.exports = nextConfig;
